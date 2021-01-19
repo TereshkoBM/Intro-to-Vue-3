2fccd461a55ee7e7a71cc5201bde74a7bc04e185
TereshkoBM <68123994+TereshkoBM@users.noreply.github.com>
2021-01-19 21:42:48 +0300

    L2-end
    Второй урок

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots' // updated data value //
+            description: 'A warm fuzzy pair of socks.' 
+        }
+    }
+})
